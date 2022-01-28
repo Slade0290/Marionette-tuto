3910ac2b9f32407a8feb8f892e1dec39f414f0e9
David LY <david@coqsenpate.com>
2022-01-28 03:07:44 +0100

    Remove ES6 syntax, first simple view hello world.

--- app/driver.js
+++ app/driver.js
@@ -1,27 +1,10 @@
-import Backbone from 'backbone'
-import Marionette from 'backbone.marionette'
+var Marionette = require('backbone.marionette')
 
-import TodoView from './views/layout'
-import TodoModel from './models/todo'
+var HelloWorld = Marionette.LayoutView.extend({
+  el: '#app-hook',
+  template: require('./templates/layout.html')
+})
 
-import MyView from './views/myview'
+var hello = new HelloWorld()
 
-const initialData = [
-  {assignee: 'Scott', text: 'Write a book about Marionette'},
-  {assignee: 'Andrew', text: 'Do some coding'}
-]
-
-export class TodoApp extends Marionette.Application {
-  onStart(options) {
-    const todoView = new TodoView({
-      collection: new Backbone.Collection(options.initialData),
-      model: new TodoModel()
-    })
-    todoView.render()
-    const myView = new MyView()
-    myView.render()
-  }
-}
-
-window.app = new TodoApp
-window.app.start({initialData: initialData})
+hello.render()
